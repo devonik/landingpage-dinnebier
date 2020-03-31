7bcf128bfa420a8a474311731ffb5772488b8507
Christoph Müller <christoph.mueller@upljft.com>
2020-03-31 16:30:20 +0200

    Install swiper
    - create css variables
    - add header and navigation

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "njk",
+    "css"
+  ]);
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    passthroughFileCopy: true,
+    templateFormats : ["njk", "md"],
+    htmlTemplateEngine : "njk",
+    markdownTemplateEngine : "njk",
+  }
+};
