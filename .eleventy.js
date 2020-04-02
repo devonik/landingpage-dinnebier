dd548bbb1cfeeb8df655786c0ba45cebed6d74a6
Christoph Müller <christoph.mueller@upljft.com>
2020-04-02 18:13:31 +0200

    Use scss, install gulp, minify styles and html

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,25 @@ module.exports = function(eleventyConfig) {
     "njk",
     "css"
   ]);
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
+
+  // pass through static files
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("js"); // to be removed when gulp concat js works
+
+  // minify the html output
+  const htmlmin = require("html-minifier");
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: false,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
 
   return {
     passthroughFileCopy: true,
