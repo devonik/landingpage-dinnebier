c19d850bbaec8619d6b349f8eeb4f523dafe3f50
Christoph Müller <christoph.mueller@upljft.com>
2020-04-02 13:56:29 +0200

    Make responsive adaptions and go mobile first

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,18 @@
 var mySwiper = new Swiper ('.swiper-container', {
-  // Optional parameters
   loop: true,
-  slidesPerView: 5,
-  spaceBetween: 50,
+  slidesPerView: 2,
+  spaceBetween: 25,
   centeredSlides: true,
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    900: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    }
   }
 })
