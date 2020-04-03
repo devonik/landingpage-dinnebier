37c93d6308f5b86d79c0315f7701cf55cc6e8b14
Christoph Müller <christoph.mueller@upljft.com>
2020-04-03 11:28:19 +0200

    Remove data files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var content = document.querySelector('.highlights__content');
+
 var selectHighlights = document.querySelector('.select__option--highlights');
 var contentHighlights = document.querySelector('.highlights__content--highlights');
 
@@ -9,15 +11,18 @@ var contentLeasing = document.querySelector('.highlights__content--leasing');
 
 selectHighlights.onclick = function() {
   console.log('click' + selectHighlights);
+  content.classList.remove('active')
   contentHighlights.classList.toggle('active');
 }
 
 selectEmission.onclick = function() {
   console.log('click' + selectEmission);
+  content.classList.remove('active')
   contentEmission.classList.toggle('active');
 }
 
 selectLeasing.onclick = function() {
   console.log('click' + selectLeasing);
+  content.classList.remove('active')
   contentLeasing.classList.toggle('active');
 }
