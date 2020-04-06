9983717636de24ca94881c021472ebb3c2c17ac8
Christoph Müller <christoph.mueller@upljft.com>
2020-04-06 15:50:38 +0200

    Add buttons to highlights desktop view

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@ var highlightsSelect = $('.highlights .select');
 var highlightsContent = $('.highlights__content');
 var highlightsContentDetails = $('.highlights__content--details');
 
-highlightsContent.hide();
-highlightsContentDetails.show();
-highlightsSelect.change(function() {
-    var value = $('.highlights .select option:selected').val();
-    var content = $('.highlights__content--' + value);
+// Mobile select
+if (parseInt($(window).width()) < 900) {
+  highlightsContent.hide();
+  highlightsContentDetails.show();
+  highlightsSelect.change(function() {
+      var value = $('.highlights .select option:selected').val();
+      var content = $('.highlights__content--' + value);
 
-    content.show();
-    content.siblings(highlightsContent).hide();
-});
+      content.show();
+      content.siblings(highlightsContent).hide();
+  });
+} else {
+  // Desktop select
+  highlightsContent.hide();
+  highlightsContentDetails.show();
+  $('.highlights__select--desktop div').click(function() {
+    var toggleIdSelect = $(this).data('select');
+    highlightsContent.hide();
+    $('.highlights__content--' + toggleIdSelect).toggle();
+    $('.highlights__select--desktop div').removeClass('is-active');
+    $(this).toggleClass('is-active');
+  });
+}
