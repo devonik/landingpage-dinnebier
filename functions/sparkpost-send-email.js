1c776dc7890b5204a1f130f7c1ef927f64c5e92c
Niklas Grieger <niklas.grieger1@gmail.com>
2020-04-07 18:11:12 +0200

    final send mail with sparkpost

--- functions/sparkpost-send-email.js
+++ functions/sparkpost-send-email.js
@@ -1,37 +1,49 @@
 const SparkPost = require('sparkpost');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
-  
-  console.log("API KEY: ",process.env.SPARKPOST_API_KEY);
 }
 const options = {
   endpoint: 'https://api.eu.sparkpost.com:443'
 };
 
 const client = new SparkPost(process.env.SPARKPOST_API_KEY,options);
-
 exports.handler = function(event, context, callback) {
-  
     const {parse} = require('querystring');
-
     let parsedData = parse(event.body);
     console.log("parsed data: ",parsedData);
+
+    let header = "<p><b>Es sind folgende Formulardaten von der Seite https://aktion.dinnebiergruppe.de/ eingegangen:</b></p><hr>"
+
     let body = "";
     for(var key in parsedData){
       var value = parsedData[key];
       body += "<p><b>"+key+": </b> <span>"+value+"</span></p>";
     }
+
   client.transmissions
     .send({
       content: {
         from: 'dinnebier@mail.ljft.de',
-        subject: 'Hello, World!',
+        subject: 'Formular',
         html:
-          "<html><body>"+body+"</body></html>"
+          "<html><body>"+header+body+"</body></html>"
       },
-      //TODO change receipent to real
-    recipients: [{ address: 'developer@upljft.com' }]
+    recipients: [{ address: 'lead.krefeld@dinnebier-gruppe.de' }, { address: 'developer@upljft.com' }]
+  }).then(result => {
+    callback(null, {
+      statusCode: 200,
+      body: "Success",
+      headers: {
+        'Content-Type': "application/json",
+      }
+    });
   }).catch(err => {
-    console.log(err);
+    callback(null, {
+      statusCode: 500,
+      body: err,
+      headers: {
+        'Content-Type': 'appl/xml',
+      },
+    });
   });
 }
